52973a66ab6bac13fbd5361fb99173942f8b094b
Collin Tod <squablesquable@gmail.com>
2019-01-18 17:40:50 -0500

    I/O minigrep project

---
+++ minigrep/src/main.rs
@@ -0,0 +1,23 @@
+extern crate minigrep;
+
+use std::env;       // For argument reading
+use std::process;   // For exit codes
+
+use minigrep::Config;
+
+fn main() {
+    let args : Vec<String> = env::args().collect();
+
+    // Either puts the Config struct into config, or reports err and exits
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}!", e);
+        process::exit(1);
+    }
+}
+
+
